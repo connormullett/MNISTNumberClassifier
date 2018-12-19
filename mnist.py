b7c48d4214401bd651c5b32daf4d21ce485f0f5c
connor <connormullett@gmail.com>
2018-12-19 14:06:14 -0500

    initial

---
+++ mnist.py
@@ -0,0 +1,31 @@
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print('loss: ', val_loss, ' val_acc: ', val_acc)
+
+predictions = model.predict(x_test)
+
+for i in range(5):
+    print('prediction {}: '.format(i + 1), np.argmax(predictions[i]))
+    plt.imshow(x_test[i])
+    plt.show()
